dd0616288c559825c49ced7696e8db00796779e8
Hankey84 <hankeybanister84@gmail.com>
2023-11-07 22:19:32 +0300

    Create clients part and filter for definitely profanity

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,20 +1,48 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import dispatcher
+from aiogram import Bot, types, Dispatcher
 from aiogram.utils import executor
 
+import json, string
 import os
 
+
+
 bot = Bot(token=os.getenv('SECRET_TOKEN'))
-dp = dispatcher(bot)
+
+dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print("Бот вышел в онлайн")
+
+
+"""*************** Clients part ***********************"""
+
+
+@dp.message_handler(commands=["start", "help"])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, "Приятного аппетита")
+        await message.delete()
+    except:
+        await message.reply("Общение с ботом через ЛС, напишите ему:\nhttps://t.me/pizza_bulka_bot")
+
+
+"""*************** Admin part *************************"""
+
+"""**************** Common part ***********************"""
+
 
 @dp.message_handler()
-async def echo_send(message : types.Message):
-    if message.text == 'Привет':
-        await message.answer('И тебе привет!') # simple reply
-    #await message.reply(message.text)  # reply to smbdy
-    #await bot.send_message(message.from_user.id, message.text)  #reply in own branch
+async def echo_send(message: types.Message):
+    # Фильтр матов, хранящихся в файле cenz.json
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}.intersection(set(json.load(open('cenz.json')))) != set():
+        await message.reply('Маты в группе запрещены')
+        await message.delete()
+    
+    #if message.text == "Привет":
+    #    await message.answer("И тебе привет!")  # simple reply
+    # await message.reply(message.text)  # reply to smbdy
+    # await bot.send_message(message.from_user.id, message.text)  #reply in own branch
 
 
-
-executor.start_polling(dp, scip_updates=True)
-
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
